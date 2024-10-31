954ffda70e6b2072753d08792edcddeed88e92b9
Chen, Liang Yu (Andymememe) <andymememe@gmail.com>
2024-10-31 15:20:42 +0800

    Add: Straming when ask question

--- sidebar/sidebar.js
+++ sidebar/sidebar.js
@@ -1,12 +1,15 @@
 var response = document.getElementById("response");
+const decoder = new TextDecoder("utf-8");
 
 document.getElementById("summary").addEventListener('click', function() {
     response.textContent = "Thinking...";
     getHightlighted()
         .then(text => {
             queryGPT("\u8acb\u5c07\u4ee5\u4e0b\u6587\u7ae0\u7684\u91cd\u9ede\u6574\u7406\u6210\u4e2d\u6587\u689d\u5217\u91cd\u9ede\uff0c\u4e26\u4e14\u6bcf\u500b\u91cd\u9ede\u90fd\u7c21\u77ed\u5448\u73fe\uff0c\u4e0d\u8981\u6709\u904e\u591a\u7684\u5197\u9918\u8d05\u5b57:\n" + text)
-            .then(text => {
-                response.textContent = text;
+            .then(reader => {
+                readStream(reader).then(null, error => {
+                    response.textContent = error;
+                });
             }, error => {
                 response.textContent = error;
             });
@@ -18,8 +21,10 @@ document.getElementById("define").addEventListener('click', function() {
     getHightlighted()
     .then(text => {
         queryGPT("\u8acb\u7528\u4e2d\u6587\u5b9a\u7fa9\u4ee5\u4e0b\u8a5e\u5f59\u6216\u7247\u8a9e\uff0c\u8acb\u5982\u8fad\u5178\u4e00\u822c\u56de\u7b54\u800c\u975e\u76f4\u63a5\u7ffb\u8b6f:\n" + text)
-        .then(text => {
-            response.textContent = text;
+        .then(reader => {
+            readStream(reader).then(null, error => {
+                response.textContent = error;
+            });
         }, error => {
             response.textContent = error;
         });
@@ -31,8 +36,10 @@ document.getElementById("translate").addEventListener('click', function() {
     getHightlighted()
     .then(text => {
         queryGPT("\u8acb\u5c07\u4ee5\u4e0b\u6587\u5b57\u7ffb\u8b6f\u6210\u4e2d\u6587\uff0c\u4e0d\u8981\u505a\u4efb\u4f55\u8d85\u8b6f\u548c\u89e3\u91cb:\n" + text)
-        .then(text => {
-            response.textContent = text;
+        .then(reader => {
+            readStream(reader).then(null, error => {
+                response.textContent = error;
+            });
         }, error => {
             response.textContent = error;
         });
@@ -45,13 +52,47 @@ document.getElementById("ask").addEventListener('click', function() {
     .then(text => {
         let quest = document.getElementById('question').value;
         queryGPT(quest + "\uff0c\u8acb\u53c3\u8003\u4ee5\u4e0b\u6587\u5b57\u505a\u56de\u61c9\uff0c\u4e0d\u8981\u7528\u5176\u4ed6\u8cc7\u6599\u4f86\u505a\u53c3\u8003:\n" + text)
-        .then(text => {
-            response.textContent = text;
+        .then(reader => {
+            readStream(reader).then(null, error => {
+                response.textContent = error;
+            });
         }, error => {
             response.textContent = error;
         });
     });
 });
+
+async function readStream(reader) {
+    response.textContent = "";
+    while(true) {
+        const { done, value } = await reader.read();
+        if (done) {
+            break;
+        }
+
+        const text = decoder.decode(value);
+        const lines = text.split("\n");
+
+        lines.forEach(line => {
+            if (line.startsWith("data:")) {
+                const jsonString = line.replace(/^data:\s*/, "")
+                if (jsonString === "[DONE]") return;
+
+                try {
+                    const jsonData = JSON.parse(jsonString);
+                    const content = jsonData.choices[0].delta.content;
+                    // Check if content is not undefined
+                    if (content!== undefined) {
+                        response.textContent += content;
+                    }
+                } catch (error) {
+                    throw error;
+                }
+            }
+        });
+    }
+    return;
+}
 
 function getHightlighted() {
     const sending = browser.runtime.sendMessage(
@@ -84,7 +125,7 @@ function queryGPT(prompt) {
     
     return storageValues.then(res => {
         if (!res) {
-            return "";
+            return new Error("No response");
         }
 
         // Extract endpoint, apikey and model from the response object
@@ -95,7 +136,7 @@ function queryGPT(prompt) {
 
         // Check if endpoint is empty
         if (!endpoint || endpoint.trim() === "") {
-            return "Please setup endpoint in setting page";
+            throw new Error("Please setup endpoint in setting page");
         }
 
         // Create the body for fetch request
@@ -103,7 +144,8 @@ function queryGPT(prompt) {
             messages: [{
                 role: "user",
                 content: prompt
-            }]
+            }],
+            stream: true
         };
         let headers = {
             'Content-Type': 'application/json; charset=utf-8'
@@ -129,15 +171,12 @@ function queryGPT(prompt) {
         // Fetch request to the OpenAI API endpoint
         return fetch(endpoint, options).then(response => {
             if (!response.ok) {
-                return `HTTP error! status: ${response.status}`;
+                throw new Error(`HTTP error! status: ${response.status}`);
             }
-            
-            let data = response.json()
-            return data.then(d => {
-                return d['choices'][0]['message']['content']; // return response as json format
-            }, error => {
-                return error;
-            })
+
+            return response.body.getReader();
+        }, error => {
+            throw error;
         });
     });
 }
